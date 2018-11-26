cee441c5ae61e5f329ae26b30c9cf58f08c134f9
lionel <lioneltolosaarmas@gmail.com>
2018-11-26 11:20:17 +0100

    ADD Search artists

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
-  styles: []
+  templateUrl: './search.component.html'
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  artists: any[] = [];
 
-  ngOnInit() {
+  constructor(private spotifyService: SpotifyService) { }
+
+  search(termino: String) {
+    console.log(termino);
+    this.spotifyService.getArtist( termino )
+      .subscribe( (data: any) => {
+        console.log(data.artists.items);
+        this.artists = data.artists.items;
+      });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,9 +14,17 @@ export class SpotifyService {
 
    getNewReleases() {
       const headers = new HttpHeaders ({
-        'Authorization': 'Bearer BQDRoWV3-y25fFmOvKN5NvFL2SeyJzBWlVIVTHsJzVqcaSj_VATeNmgH7wVcsm14lyVm73oGP9WzSZZLLBo'
+        'Authorization': 'Bearer BQBWAInaPrvR1Q7glTL62c1-xhd_1QYLXNVjQZ5Z0Iuc1dfgXQmqiW7EDZRJiNqAM3-ufvxFJNFrtvbBo3Q'
       });
 
       return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=40', { headers });
+    }
+
+    getArtist(termino: String) {
+      const headers = new HttpHeaders ({
+        'Authorization': 'Bearer BQBWAInaPrvR1Q7glTL62c1-xhd_1QYLXNVjQZ5Z0Iuc1dfgXQmqiW7EDZRJiNqAM3-ufvxFJNFrtvbBo3Q'
+      });
+
+      return this.http.get(`https://api.spotify.com/v1/search?query=${ termino }&type=artist&market=ES&offset=0&limit=15`, { headers });
     }
 }
